c24b9738d6606a1b459bb4f690da42fdbf472f03
RodrigoC28 <rodrigoscosta28@gmail.com>
2025-04-03 09:26:25 +0100

    reorganização

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,18 @@
-/* importar o módulo do EXPRESS */
-const express = require("express");
-const app = express(); //definir a nossa app em EXPRESS
-const session = require("express-session");
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = 5000
 
-//definir a pasta assets como estática (um género de diretoria pública)
-app.use(express.static("./assets"));
+app.set("port", process.env.PORT || port);
+app.use(express.json());
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true }));
 
-//configurações do SERVER
-app.set("port", process.env.port || process.env.PORT || 5000);
-app.use(express.urlencoded({ extended: true })); //permitir pedidos do exterior
+app.use("", require(""));
+app.use("", require(""));
+app.use("", require(""));
 
-app.use(
-  session({
-    secret: ")>S*g;#RMp|@UzT*eh<$Q>qB3'Amu&", // Replace with a secure key
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
-app.use((req, res, next) => {
-res.locals.user = req.session.user || null; // atrivuir uma sessão utilizador ou não
-next();
-});
-
-//rotas
-app.use("/", require("./routes/main.route"));
-
-app.use("/", require("./routes/utilizador.route"));
-app.use("/", require("./routes/viagem.route"));
-app.use("/", require("./routes/paragem.route"));
-app.use("/", require("./routes/viagem_utilizador.route"));
-app.use("/", require("./routes/viagem_paragem.route"));
-app.use("/", require("./routes/autocarro.route"));
-
-//instancia e inicia o servidor
 app.listen(app.get("port"), () => {
-  console.log("Servidor iniciado na porta: " + app.get("port"));
+    console.log("Servidor a correr na porta "+app.get("port"));
 });
